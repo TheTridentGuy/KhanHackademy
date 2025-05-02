6310fc041e268e417bc8eb6e68dab5e3156609c1
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-05-01 22:26:51 -0700

    add userscript header

---
+++ header.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name          	Khan Hackademy
+// @description     Khan Hackademy is a script that gets a full score on every Khan Academy exercise you complete.
+// @author			TheTridentGuy (thetridentguy.com)
+// @namespace       thetridentguy
+// @license         GPL v3 - http://www.gnu.org/licenses/gpl-3.0.txt
+// @copyright       Copyright (C) 2025 Aiden Bohlander aka. TheTridentGuy
+// @include         https://khanacademy.org/*
+// @version         1.0
+// @run-at			document-end
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* 
+ This file is a template, the contents of `bookmarklet.js` go here.
+*/
